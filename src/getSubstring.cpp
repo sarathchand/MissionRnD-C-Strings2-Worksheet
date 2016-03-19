afbca97762248cfe974328878b58ce3e8db6ac1e
sarathchand <sarathchand@missionrnd.com>
2016-03-19 10:15:15 +0530

    Submit Commit

--- src/getSubstring.cpp
+++ src/getSubstring.cpp
@@ -16,8 +16,54 @@ original String
 
 #include <stddef.h>
 #include <stdlib.h>
+int validate(char *str, int i, int j);
 
 char * get_sub_string(char *str, int i, int j){
 
-    return NULL;
+	int p, q, r = 0;
+	char *str1;
+	q = validate(str, i, j);
+	if (q > 0)
+	{
+		for (p = 0; str[p] != '\0'; p++);
+		if (i < 0)
+		{
+			i = 0;
+		}
+		if (j>p)
+		{
+			j = p - 1;
+		}
+		str1 = (char*)malloc(sizeof(char)*100);
+		for (i = i; i <= j; i++)
+		{
+			str1[r] = str[i];
+			r++;
+		}
+		str1[r] = '\0';
+		return str1;
+
+	}
+	else
+	{
+
+		return NULL;
+	}
+}
+int validate(char *str, int i, int j)
+{
+	int p;
+	if (str == NULL)
+	{
+		return -1;
+	}
+	if (i > j)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1;
+	}
+
 }
